962baa3a8690e32f51bd902179abc352a5fbe6ac
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2024-01-14 22:27:55 -0800

    Add back in missing dependency

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 import json
 import locale
 
